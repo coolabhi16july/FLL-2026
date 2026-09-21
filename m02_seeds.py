dd8a54381092733d3471a0a18b68488da884a0c9
coolabhi16july <abhisheksrivastava16@outlook.com>
2026-09-21 19:25:49 -0400

    Add m02_seeds.py file

--- m02_seeds.py
+++ m02_seeds.py
@@ -8,7 +8,7 @@ from base_robot import PybricksBot
 from pybricks.tools import wait
 
 
-def run_mission_02():
+def run_mission_10():
     bot = PybricksBot()
     try:
         print("[Mission 2] Starting")
@@ -17,14 +17,14 @@ def run_mission_02():
         pass
 
             # TODO: implement real sequence
-    bot.drive_forward(74, 200)
-    bot.turn_left(15, 100)
-    bot.drive_forward(2, 200)
-    bot.turn_left(30, 150)
-    bot.turn_right(45, 150)
-    bot.drive_backward(65, 500)
+
+    bot.drive_forward(6.56, 200)
+    bot.turn_right(42, 100)
+    bot.drive_forward(32, 200)
+    bot.move_right_arm(300, 150)
+    bot.drive_backward(30, 200)
     try:
-        print("[Mission 2] Complete")
+        print("[Mission 1] Complete")
         bot.hub.speaker.beep(frequency=1000, duration=200)
     except Exception:
         pass
